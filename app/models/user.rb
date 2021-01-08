86647e88a06f6d1f5eef2a5b1c41346d3de3d195
RAFH82 <hrechkar@gmail.com>
2021-01-07 19:05:23 -0800

    Completed Login/Signup user auth

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  # validates :first_name, last_name, email, password_digest, presence: true
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  
+  # resources :users, only: [:new, :create]
+  # resources :sessions, only: [:new, :create, :destroy]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
